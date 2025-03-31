5e4f354cdd5e80bdca6bf672b65d5870eed40dfd
Vlad <142720001+StinflixWork@users.noreply.github.com>
2025-03-31 19:30:06 +0300

    fix: add proxy for dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
 	const env = loadEnv(mode, process.cwd(), '')
+	const isDev = command === 'serve'
 
 	return {
 		plugins: [react()],
 		define: {
-			__API__: JSON.stringify('/api')
+			__API__: JSON.stringify(isDev ? '/api' : env.VITE_API_URL)
 		},
 		resolve: {
 			alias: {
