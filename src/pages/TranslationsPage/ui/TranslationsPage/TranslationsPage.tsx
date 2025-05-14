23935ed6b8f7c9908b434cd84f2e38d292450f01
dipflix <vakardhuk.dmitriy.03@gmail.com>
2025-05-14 18:10:31 +0300

    save

--- src/pages/TranslationsPage/ui/TranslationsPage/TranslationsPage.tsx
+++ src/pages/TranslationsPage/ui/TranslationsPage/TranslationsPage.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import { Accordion, AccordionItem } from '@heroui/accordion'
+import { Button } from '@heroui/button'
+import { Input, Textarea} from '@heroui/input'
 import { useGetTranslationsQuery } from '@/entities/Translations/api/translationsApi.ts'
 import { AppLoader } from '@/shared/ui/AppLoader'
 import styles from './TranslationsPage.module.scss'
@@ -14,19 +17,33 @@ export const TranslationsPage = () => {
 	return (
 		<div className={styles.root}>
 			<h1>Translations Page</h1>
-			{data?.data.map(translation => (
-				<div key={translation.key}>
-					<h2>{translation.key}</h2>
-					<ul>
-						{Object.entries(translation.translations).map(([locale, value]) => (
-							<li key={locale}>
-								{locale}: {value}
-							</li>
-						))}
-					</ul>
-					<br />
-				</div>
-			))}
+			<Input label='Search' type='text' />
+			<Accordion isCompact>
+				{data?.data.map(e => (
+					<AccordionItem title={e.key} key={e.key}>
+						<Input label='key' type='text' value={e.key} />
+						<br />
+						<table className={styles.translationTable}>
+							<thead>
+								<tr>
+									<th>Language</th>
+									<th>Translation</th>
+								</tr>
+							</thead>
+							<tbody>
+								{Object.entries(e.translations).map(([lang, text]) => (
+									<tr key={lang}>
+										<td>{lang}</td>
+										<td>	<Textarea type='text' value={text} /></td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+						<br />
+						<Button color='danger'>Delete</Button>
+					</AccordionItem>
+				))}
+			</Accordion>
 		</div>
 	)
 }
